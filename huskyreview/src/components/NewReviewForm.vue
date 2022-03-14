7f17e0ed09119f26997c3592f25af6422c6423ec
mezitta <michaelezitta@gmail.com>
2022-03-14 09:10:25 -0400

    added basic form

--- huskyreview/src/components/NewReviewForm.vue
+++ huskyreview/src/components/NewReviewForm.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div>
+        form goes here
+    </div>
 </template>
 
 <script>
--- huskyreview/src/views/NewReview.vue
+++ huskyreview/src/views/NewReview.vue
@@ -1,13 +1,68 @@
 <template>
     <div class="review">
         <p>review page</p>
+        <ReviewForm />
+        <form action="">
+            <div class="container-fluid">
+                <div class="row">
+                    <label for="">Title</label><br>
+                    <input type="text" required v-model="title">
+                </div>
+
+                <div class="row">
+                    <label for="">Professor</label><br>
+                    <input type="text" required v-model="professor">
+                </div>
+
+                <div class="row">
+                    <label for="">Class</label><br>
+                    <input type="text" required v-model="className">
+                </div>
+
+                <div class="row">
+                    <label for="">Review</label><br>
+                    <input type="text" required v-model="review">
+                </div>
+
+                <div class="row">
+                    <label for="">Rating</label><br>
+                    <select required v-model="rating">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+
+                <div class="row">
+                    <button>Create Review</button>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'New-Review',
-    };
+// import axios from 'axios';
+import ReviewForm from "../components/NewReviewForm"
+
+export default {
+    name: 'New-Review',
+    components:{
+        ReviewForm
+    },
+    data() {
+        return {
+            title: '',
+            professor: '',
+            className: '',
+            reivew: '',
+            rating: ''
+        }
+    }
+};
+
 </script>
 
 <style>
