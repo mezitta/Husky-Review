66d952df1f01c2bcf8e9fb7b539a362d23d2ba13
Jacob Smith <98979944+jcsmith2@users.noreply.github.com>
2022-03-14 14:20:07 -0400

    test/Test -> review/Review
    Plus some documentation.

---
+++ huskyreview/backend/routes/review.route.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { nextTick } = require('vue');
+const reviewRoute = express.Router();
+
+let ReviewModel = require('../models/review');
+
+// TODO: Filter reviews by query string.
+reviewRoute.route('/').get((req, res) => {
+    ReviewModel.find((error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+// TODO: Insert incoming data into database.
+reviewRoute.route('/add-review').post((req, res) => {
+    ReviewModel.create(req.body, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+// TODO: Prevent updating anything beyond like/dislike tallies.
+reviewRoute.route('/update-review/:id').put((req, res) => {
+    ReviewModel.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+// TODO: Remove? Client cannot be trusted to moderate.
+reviewRoute.route('/delete-review/:id').delete((req, res, next) => {
+    ReviewModel.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.status(200).json({
+                msg: data
+            })
+        }
+    })
+})
+
+module.exports = reviewRoute;
