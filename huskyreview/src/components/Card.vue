794db975901119d62856e099a1c2b37a561493fc
Ben Walby <bhwalby@mtu.edu>
2022-03-18 13:15:26 -0400

    changed card.vue

--- huskyreview/src/components/Card.vue
+++ huskyreview/src/components/Card.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <div class="container card" >
         <div class="row justify-content-end bottom-margin top-row-margin">
@@ -77,84 +76,4 @@ export default {
         margin-bottom:0px; 
     }
 
-=======
-<template>
-    <div class="container card" >
-        <div class="row justify-content-end bottom-margin top-row-margin">
-            <div class="col">
-                title
-            </div>
-            <div class="col-2 text-end">
-                rating
-            </div>
-        </div>
-        <div class="row bottom-margin">
-            <div class="col right-border">
-                class name
-            </div>
-            <div class="col right-border">
-                class id
-            </div>
-            <div class="col">
-                prof name
-            </div>
-        </div>
-        <div class="row bottom-margin">
-            <div class="col">
-                body
-            </div>
-        </div>
-        <div class="row justify-content-end bottom-row-margin">
-            <div class="col-3">
-                <p class="text-end">
-                    credibility
-                </p>
-            </div>
-            <div class="col-1">
-                <img src="../assets/up.png" alt="" width="15" height="15" />
-            </div>
-            <div class="col-1">
-                0
-            </div>
-            <div class="col-1">
-                <img src="../assets/down.png" alt="" width="15" height="15" />
-            </div>
-            <div class="col-1">
-                0
-            </div>
-        </div>
-    </div>
-</template>
-
-<script>
-export default {
-    name: "review-card",
-}
-</script>
-
-<style scoped>
-
-    .card {
-        background-color: rgb(255,255,255);
-    }
-
-    .right-border {
-         padding-right: 5px; 
-         border-right: 1px solid rgb(0, 0, 0);
-    }
-    
-    .bottom-margin { 
-        margin-bottom:0px; 
-        border-bottom: 1px solid rgb(0, 0, 0);
-    }
-
-    .top-row-margin {
-        margin-top: 10px;
-    }
-
-    .bottom-row-margin { 
-        margin-bottom:0px; 
-    }
-
->>>>>>> 97897752e3d9f7a5ade31b5e1e92819ee6550cf3
 </style>
